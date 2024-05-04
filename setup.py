8e6ca33e4355596f0991144e799011e3b97a3829
Liam-G-Cantoyono <130859695+Liam-G-Cantoyono@users.noreply.github.com>
2024-05-04 20:15:51 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name = "remocolab.py",
     version = "0.1",
     py_modules = ['remocolab'],
-    url = "https://github.com/cdkfox/cdkcolab",
+    url = "https://github.com/Liam-G-Cantoyono/SS",
     author = "cdkfox",
 )
